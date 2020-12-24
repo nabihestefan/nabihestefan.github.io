6302bf4206496139bc07fd5010083e38c7d4dc50
Nabih <estefan.nabih@gmail.com>
2020-12-23 20:43:34 -0500

    updated QR code, redeploying site Dec 23

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,24 @@
 set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-# Add changes to git.
-git add -A
-
-# Commit changes.
+# check message
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
+#commit to Portfolio repo
+git add -A
+git commit -m "$msg"
+git push
+
+# Build the project.
+hugo -t hugo-resume # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# commit to site repo
+git add -A
+git commit -m "$msg"
 git push
