cf15eb97162b4314f1b83cd4a76abffb3e8a31fa
Nabih <estefan.nabih@gmail.com>
2021-04-08 21:10:20 -0400

    rebuilding site Thu 08 Apr 2021 09:10:20 PM EDT

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
+# If a command fails then the deploy stops
 set -e
-export HUGO_SHORT_COMMIT=`git rev-parse --short HEAD`
 
-if [ "`git status -s`" ]
-then
-        echo "The working directory is dirty. Please commit any pending changes."
-            exit 1;
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# check message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+#commit to Portfolio repo
+git add -A
+git commit -m "$msg"
+git push upstream main
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B master public origin/main
+printf "\033[0;32mPushed to Portfolio\033[0m\n"
 
-echo "Removing existing files"
-rm -rf public/*
+# Build the project.
+hugo -t hugo-resume # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-echo "Generating site"
-hugo --minify
-
-echo "Updating master pages branch"
-cd public
-
-git add --all && git commit -m "Deploy changes"
-
-#echo "Pushing to github"
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+# Go To Public folder
+cp -r /public/. ../Website/
+cd ../Website/
+# Add name file for when i get my domain
+# echo "www.nabihestefan.co" > CNAME
+# commit to site repo
+git add .
+git commit -m "$msg"
 git push origin main
+
+printf "\033[0;32mPushed to Public\033[0m\n"
 
 cd ..
