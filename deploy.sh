ff5a15bd4c6c4c1ed632ca85c2502df4b45b91d6
Nabih <estefan.nabih@gmail.com>
2021-10-14 19:54:36 -0400

    rebuilding site Thu 14 Oct 2021 07:54:36 PM EDT

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[0;32mDeploying Portfolio Updates to GitHub...\033[0m\n"
 # check message
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
@@ -20,4 +20,22 @@ printf "\033[0;32mPushed to Portfolio\033[0m\n"
 # Build the project.
 hugo -t hugo-resume
 
-## To publish, copy public folder into other repo and push that repo
+## Publish
+cd public/
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying Public Updates to GitHub...\033[0m\n"
+# check message
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+#commit to Portfolio repo
+git add -A
+git commit -m "$msg"
+git push origin main
+
+printf "\033[0;32mPushed to Public\033[0m\n"
